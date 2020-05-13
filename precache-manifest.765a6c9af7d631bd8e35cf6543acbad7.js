ba7754f68238103883ad49168501037596990330
Mykhaylo1992 <misha.dyatel@gmail.com>
2020-05-13 15:42:55 +0300

    Updates

---
+++ precache-manifest.765a6c9af7d631bd8e35cf6543acbad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff59bf0bf8dfed7b0498cc1b83e1945a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "659d995d80a58fc65245",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9c8ae13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9c8ae13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d4bd0c28567b84a4dd",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3a3c14cd.chunk.js"
+  },
+  {
+    "revision": "530a2ea63a63b985e80c",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f97b67d2.js"
+  }
+]);
